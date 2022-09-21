1dd06efe0c04e464fd647a54fdf1607f052671ea
Bryce Melander <brycemelander@gmail.com>
2022-09-21 15:26:09 -0700

    updated

--- Lab0/Lab00.java
+++ Lab0/Lab00.java
@@ -1,7 +1,40 @@
-public class Lab00
-{
-   public static void main(String[] args)
-   {
+public class Lab00{
 
-   }
+    public static int char_count(String s, char c){
+        int count = 0;
+        char s_array[] = s.toCharArray();
+
+        for(int j = 0; j < s.length(); j++){
+            if (s_array[j] == c){
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args){
+        // Declaring and initializing some variables
+        int x = 5;
+        String y = "hello";
+        double z = 9.8;
+
+        // Printing the variables
+        System.out.println("x: " + x + " y: " + y + " z: " + z);
+
+        // An array
+        int nums[] = {3, 6, -1, 2};
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
+
+        // Call a function
+        int numFound = char_count(y, 'l');
+        System.out.println("Found: " + numFound);
+
+        // A counting for loop
+        for (int i = 1; i < 11; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
