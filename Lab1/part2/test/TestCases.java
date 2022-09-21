b4100ae23e1d6d3a4517a9e739077641b71cb549
Bryce Melander <brycemelander@gmail.com>
2022-09-21 15:30:59 -0700

    added Lab1

---
+++ Lab1/part2/test/TestCases.java
@@ -0,0 +1,28 @@
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class TestCases
+{
+   /*
+    * This test is just to get you started.
+    */
+   @Test
+   public void testGetName()
+   {
+      // This will not compile until you implement the Applicant class
+      List<CourseGrade> grades = Arrays.asList(
+         new CourseGrade("Intro to CS", 100),
+         new CourseGrade("Data Structures", 95),
+         new CourseGrade("Algorithms", 91),
+         new CourseGrade("Computer Organization", 91),
+         new CourseGrade("Operating Systems", 75),
+         new CourseGrade("Non-CS", 83)
+      );
+      Applicant testApplicant = new Applicant("Aakash", grades);
+      assertEquals("Aakash", testApplicant.getName());
+   }
+}
