8aae3b7e3ae1dde6db1bd458ccf30b81b29b9c31
Bryce Melander <brycemelander@gmail.com>
2022-09-28 08:24:09 -0700

    updated

--- Lab1/part2/src/Applicant.java
+++ Lab1/part2/src/Applicant.java
@@ -6,32 +6,37 @@ public class Applicant {
     private String background;
     private double shoe_size;
 
-    public Applicant(String name, List<CourseGrade> grades, String background, double shoe_size){
+    public Applicant(String name, List<CourseGrade> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Applicant(String name, List<CourseGrade> grades, String background, double shoe_size) {
         this.name = name;
         this.grades = grades;
         this.background = background;
         this.shoe_size = shoe_size;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public List<CourseGrade> getGrades(){
+    public List<CourseGrade> getGrades() {
         return this.grades;
     }
 
-    public String getBackground(){
+    public String getBackground() {
         return this.background;
     }
 
-    public double getShoeSize(){
+    public double getShoeSize() {
         return this.shoe_size;
     }
 
-    public CourseGrade getGradeFor(String course){
-        for (CourseGrade courseGrade : this.grades){
-            if (courseGrade.getCourseName().equals(course)){
+    public CourseGrade getGradeFor(String course) {
+        for (CourseGrade courseGrade : this.grades) {
+            if (courseGrade.getCourseName().equals(course)) {
                 return courseGrade;
             }
         }
